2870f6d81ea1985fd959d2ee106ebd2638fd0ac5
CodeLionX <sebastian.schmidl@t-online.de>
2022-04-04 18:06:51 +0200

    feat: correctly display and parse algorithm parameters

--- timeeval_gui/pages/__init__.py
+++ timeeval_gui/pages/__init__.py
@@ -10,16 +10,17 @@ class Pages:
     def __init__(self, *pages: Page):
         assert len(pages) > 0, "You have to add at least one Page"
         self.pages = pages
+        self.first_page = self.pages[0].name
+        st.session_state.setdefault("page", self.first_page)
 
     def _render_sidebar(self):
         for page in self.pages:
             name = page.name
             if st.sidebar.button(name):
-                st.session_state['page'] = name
+                st.session_state["page"] = name
 
     def _render_page(self):
-        first_page = self.pages[0].name
-        active_page = st.session_state.get("page", first_page)
+        active_page = st.session_state["page"]
         for page in self.pages:
             if active_page == page.name:
                 page.render()
--- timeeval_gui/pages/eval.py
+++ timeeval_gui/pages/eval.py
@@ -1,12 +1,16 @@
-from typing import List
+from typing import List, Dict, Any, Optional, Union
 
 import numpy as np
 import pandas as pd
 import streamlit as st
 import time
+
+from streamlit.state import NoValue
+
 from .page import Page
 
 from timeeval import Algorithm
+from timeeval.params import FixedParameters, FullParameterGrid, IndependentParameterGrid
 from timeeval_experiments import algorithms as timeeval_algorithms
 
 # keep this import!
@@ -14,6 +18,94 @@ from timeeval_experiments.algorithms import *
 
 
 algos: List[Algorithm] = [eval(f"{a}()") for a in dir(timeeval_algorithms) if "__" not in a]
+
+for algo in algos:
+    st.session_state.setdefault(f"eval-{algo.name}-n_params", 0)
+
+
+def inc_n_params(algo: str):
+    value = st.session_state.get(f"eval-{algo}-n_params", 0)
+    st.session_state[f"eval-{algo}-n_params"] = value + 1
+
+
+def dec_n_params(algo: str):
+    value = st.session_state.get(f"eval-{algo}-n_params", 0)
+    if value > 0:
+        st.session_state[f"eval-{algo}-n_params"] = value - 1
+
+
+def parse_enum_param_type(tpe: str) -> List[str]:
+    option_str = tpe.split("[")[1].split("]")[0]
+    return option_str.split(",")
+
+
+def parse_list_value(tpe: str, value: str) -> List[Union[float, int, str, bool]]:
+    subtype = tpe.split("[")[1].split("]")[0].lower()
+    cc_subtype = {
+        "int": int,
+        "float": float,
+        "bool": bool
+    }
+    value_str = value.split("[")[1].split("]")[0]
+
+    values = value_str.split(",")
+    return [cc_subtype.get(subtype, str)(v) for v in values]
+
+
+def param_input(param_config: Dict[str, Any], key: Optional[str] = None) -> Any:
+    tpe = param_config["type"]
+    label = "Value"
+
+    def get_default_value(cc) -> Any:
+        value = param_config["defaultValue"]
+        if value is None:
+            return NoValue()
+        else:
+            return cc(value)
+
+    if tpe.lower() == "int":
+        value = int(st.number_input(label,
+                                    value=get_default_value(int),
+                                    help=param_config["description"],
+                                    step=1,
+                                    key=key))
+    elif tpe.lower() == "float":
+        value = st.number_input(label,
+                                value=get_default_value(float),
+                                help=param_config["description"],
+                                step=None,
+                                format="%f",
+                                key=key)
+    elif tpe.lower().startswith("bool"):
+        st.markdown("Value")
+        value = st.checkbox("",
+                            value=get_default_value(bool),
+                            help=param_config["description"],
+                            key=key)
+    elif tpe.lower().startswith("enum"):
+        try:
+            default_index = parse_enum_param_type(tpe).index(param_config["defaultValue"])
+        except ValueError:
+            default_index = 0
+        value = st.selectbox(label,
+                             options=parse_enum_param_type(tpe),
+                             index=default_index,
+                             help=param_config["description"],
+                             key=key)
+    elif tpe.lower().startswith("list"):
+        value = st.text_input(label + f" ({tpe.lower()})",
+                              value=param_config["defaultValue"],
+                              help=param_config["description"],
+                              key=key)
+        value = parse_list_value(tpe, value)
+        print(value)
+    # elif tpe.lower() == "string":
+    else:
+        value = st.text_input(label,
+                              value=param_config["defaultValue"],
+                              help=param_config["description"],
+                              key=key)
+    return value
 
 
 class EvalPage(Page):
@@ -25,33 +117,72 @@ class EvalPage(Page):
         st.image("images/timeeval.png")
         st.title("Eval")
 
-        col1, col2 = st.columns(2)
-        with col1:
-            st.write("## Algorithms")
+        # col1, col2 = st.columns(2)
+        # with col1:
+        st.write("## Algorithms")
 
-            algorithms: List[Algorithm] = st.multiselect("Algorithms", options=algos, format_func=lambda a: a.name)
+        algo_names: List[str] = st.multiselect("Algorithms", options=[a.name for a in algos])
+        algorithms = [a for a in algos if a.name in algo_names]
 
-            st.write("### Parameters")
+        st.write("### Parameters")
 
-            for algorithm in algorithms:
-                algo_name = algorithm.name
-                st.write(f"#### {algo_name}")
-                n_param = st.number_input(f"#Parameter Settings", key=f"{algo_name}#params", min_value=0)
-                for p in range(n_param):
-                    with st.expander(f"{algo_name} - Parameter Setting #{p + 1}"):
-                        for param in algorithm.param_schema:
-                            st.text_input(algorithm.param_schema[param]["name"],
-                                          value=algorithm.param_schema[param]["defaultValue"],
-                                          help=algorithm.param_schema[param]["description"])
+        for algorithm in algorithms:
+            algo_name = algorithm.name
+            with st.expander(algo_name):
+                if not algorithm.param_schema:
+                    st.info("Algorithm has no parameters.")
+                    continue
 
-                        # todo: unfake
-                        # st.number_input("window_size", key=f"ws-{algo_name}-{p}", min_value=1)
-                        # st.number_input("n_clusters", key=f"nc-{algo_name}-{p}", min_value=1)
+                param_config_tpe = st.selectbox("Parameter configuration type",
+                                                [FixedParameters, FullParameterGrid, IndependentParameterGrid],
+                                                format_func=lambda x: x.__name__,
+                                                help="FixedParameters - Single parameters setting with one value for each.\n"
+                                                     "FullParameterGrid - Grid of parameters with a discrete number of "
+                                                     "values for each. Yields the full cartesian product of all "
+                                                     "available parameter combinations.\n"
+                                                     "IndependentParameterGrid - Grid of parameters with a discrete "
+                                                     "number of values for each. The parameters in the dict are "
+                                                     "considered independent and explored one after the other.",
+                                                key=f"eval-{algo_name}-config-tpe")
+                st.write("---")
 
-        with col2:
-            st.write("## Datasets")
+                if param_config_tpe.__name__ == FixedParameters.__name__:
+                    n_params = st.session_state.get(f"eval-{algo_name}-n_params", 0)
+                    displayed_params = []
+                    param_grid = {}
+                    for i in range(n_params):
+                        param_col1, param_col2 = st.columns((2, 1))
+                        with param_col1:
+                            param = st.selectbox("Parameter",
+                                                 options=[param for param in algorithm.param_schema if
+                                                          param not in displayed_params],
+                                                 format_func=lambda p: algorithm.param_schema[p]["name"],
+                                                 key=f"{algo_name}-parameter-name-{i}")
+                        with param_col2:
+                            value = param_input(algorithm.param_schema[param], key=f"{algo_name}-parameter-value-{i}")
+                        displayed_params.append(param)
+                        param_grid[param] = value
 
-            datasets = st.multiselect("Datasets", options=["Traffic", "ECG", "ecg-10000-1"])
+                    bt_col1, bt_col2, _ = st.columns((1, 1, 18))
+                    with bt_col1:
+                        st.button("-",
+                                  help="Remove a parameter configuration",
+                                  on_click=dec_n_params, args=[algo_name],
+                                  key=f"eval-{algo_name}-button-")
+                    with bt_col2:
+                        st.button("+",
+                                  help="Add a parameter configuration",
+                                  on_click=inc_n_params, args=[algo_name],
+                                  key=f"eval-{algo_name}-button+")
+                    algo.param_config = FixedParameters(param_grid)
+
+                else:
+                    st.error("Not implemented yet!")
+
+        # with col2:
+        st.write("## Datasets")
+
+        datasets = st.multiselect("Datasets", options=["Traffic", "ECG", "ecg-10000-1"])
 
         st.write("## General Settings")
 
